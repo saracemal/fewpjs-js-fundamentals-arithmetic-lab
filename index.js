18f60204b453d8f983cc5357c12c4905febca209
Sara Cemal <you@example.com>
2020-12-10 09:48:27 -0500

    done

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const num1 = 2
+const num2 = 31
 
-// Write your code here
+const multiply = num1 * num2
+
+const random = Math.random()
+
+const mod = 16 % 6
+
+const max = Math.max(1, 4, 8, 10, 20)
